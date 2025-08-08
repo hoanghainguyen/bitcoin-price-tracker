1b9afedc2164ab94aa017b8b5cf11ee8902ef639
Hoang Hai Nguyen <haics1002@gmail.com>
2025-08-08 10:08:02 +0200

    adapt AboutUs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,9 @@ function About() {
     <section id="about" className="max-w-7xl mx-auto p-6 bg-gray-50">
       <h2 className="text-3xl font-bold mb-4 text-gray-800">About This Project</h2>
       <p className="text-gray-600 leading-relaxed">
-        This Bitcoin Daily Price Tracker fetches historical Bitcoin prices for the past 7 days
-        using the CoinGecko API. Built with React 19 and Vite, it demonstrates API integration,
+        This Bitcoin  Price Tracker fetches historical Bitcoin prices by month since  November 2011. Built with React 19 and Vite, it demonstrates API integration,
         state management with hooks, and responsive design. Check out the code on GitHub or
-        connect with me (Hoang Hai Nguyen )to learn more!
+        connect with me <a href="https://my-portfolio-gules-alpha-89.vercel.app/> (Hoang Hai Nguyen )</a> to learn more!
       </p>
     </section>
   );
