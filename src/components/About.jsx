f56c0133fa7c60b02df8d9781afb6e9955292365
Hoang Hai Nguyen <haics1002@gmail.com>
2025-08-08 10:23:08 +0200

    adapt About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,10 @@ function About() {
   return (
     <section id="about" className="max-w-7xl mx-auto p-6 bg-gray-50">
       <h2 className="text-3xl font-bold mb-4 text-gray-800">About This Project</h2>
-      <p className="text-gray-600 leading-relaxed">
-        This Bitcoin  Price Tracker fetches historical Bitcoin prices by month since  November 2011. Built with React 19 and Vite, it demonstrates API integration,
-        state management with hooks, and responsive design. Check out the code on GitHub or
-        connect with me <a href="https://my-portfolio-gules-alpha-89.vercel.app/> (Hoang Hai Nguyen )</a> to learn more!
+		  <p className="text-gray-600 leading-relaxed">
+		  Deze Bitcoin Price Tracker haalt historische Bitcoin-prijzen op per maand sinds november 2011. Gebouwd met React 19 en Vite, demonstreert het API-integratie,
+		  statusbeheer met hooks en responsief design. Bekijk de code op GitHub of
+		  neem contact met mij <a href="https://my-portfolio-gules-alpha-89.vercel.app" /> (Hoang Hai Nguyen )</a>   op voor meer informatie!       
       </p>
     </section>
   );
